1dc2fc4ec662acb38a11b1f7ebc15b7d87d36ff2
Mikael Rasmussen <mikael@rubixy.com>
2018-12-14 23:07:14 +0100

    Add project files.

---
+++ RoadGenerator2000/Program.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace RoadGenerator2000
+{
+	class Program
+	{
+		static void Main(string[] args)
+		{
+		}
+	}
+}
